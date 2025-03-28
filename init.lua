cc09372c535d3a57f9bef2e432c7b760da7221f2
mukkid <mukkid@gmail.com>
2025-03-27 23:49:33 -0500

    Update init.lua

--- init.lua
+++ init.lua
@@ -2,237 +2,283 @@ require("kidambim")
 
 local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 if not vim.loop.fs_stat(lazypath) then
-	vim.fn.system({
-		"git",
-		"clone",
-		"--filter=blob:none",
-		"https://github.com/folke/lazy.nvim.git",
-		"--branch=stable",
-		lazypath,
-	})
+    vim.fn.system({
+        "git",
+        "clone",
+        "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable",
+        lazypath,
+    })
 end
 vim.opt.rtp:prepend(lazypath)
 require("lazy").setup({
-	-- your plugins will go here
-	{
-		{
-			"windwp/nvim-autopairs",
-			event = "InsertEnter",
-			config = true
-		},
-		"folke/which-key.nvim",
-		event = "VeryLazy",
-		opts = {
-			-- your configuration comes here
-			-- or leave it empty to use the default settings
-			-- refer to the configuration section below
-		},
-		keys = {
-			{
-				"<leader>?",
-				function()
-					require("which-key").show({ global = false })
-				end,
-				desc = "Buffer Local Keymaps (which-key)",
-			},
-		},
-	},
-	{
-		"kylechui/nvim-surround",
-		version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
-		event = "VeryLazy",
-		config = function()
-			require("nvim-surround").setup({
-				-- Configuration here, or leave empty to use defaults
-			})
-		end
-	},
-	{
-		"max397574/better-escape.nvim",
-		config = function()
-			require("better_escape").setup()
-		end,
-	},
-	{
-		"nvim-tree/nvim-tree.lua",
-		version = "*",
-		lazy = false,
-		dependencies = {
-			"nvim-tree/nvim-web-devicons",
-		},
-		config = function()
-			require("nvim-tree").setup {}
-		end
-	},
+    -- your plugins will go here
+    {
+        'windwp/nvim-autopairs',
+        event = "InsertEnter",
+        config = true
+        -- use opts = {} for passing setup options
+        -- this is equivalent to setup({}) function
+    },
+    {
+        "fasterius/simple-zoom.nvim",
+        opts = {
+            hide_tabline = true
+        },
+    },
+    {
+        "nwiizo/cargo.nvim",
+        build = "cargo build --release",
+        config = function()
+            require("cargo").setup({
+                float_window = true,
+                window_width = 0.8,
+                window_height = 0.8,
+                border = "rounded",
+                auto_close = true,
+                close_timeout = 5000,
+            })
+        end,
+        ft = { "rust" },
+        cmd = {
+            "CargoBench",
+            "CargoBuild", 
+            "CargoClean",
+            "CargoDoc",
+            "CargoNew",
+            "CargoRun",
+            "CargoTest",
+            "CargoUpdate"
+        }
+    },
+    {
+        "folke/which-key.nvim",
+        event = "VeryLazy",
+        opts = {
+            -- your configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        },
+        keys = {
+            {
+                "<leader>?",
+                function()
+                    require("which-key").show({ global = false })
+                end,
+                desc = "Buffer Local Keymaps (which-key)",
+            },
+        },
+    },
+    {
+        "kylechui/nvim-surround",
+        version = "^3.0.0", -- Use for stability; omit to use `main` branch for the latest features
+        event = "VeryLazy",
+        config = function()
+            require("nvim-surround").setup({
+                -- Configuration here, or leave empty to use defaults
+            })
+        end
+    },
+    {
+        "max397574/better-escape.nvim",
+        config = function()
+            require("better_escape").setup()
+        end,
+    },
+    {
+        "nvim-tree/nvim-tree.lua",
+        version = "*",
+        lazy = false,
+        dependencies = {
+            "nvim-tree/nvim-web-devicons",
+        },
+        config = function()
+            require("nvim-tree").setup {}
+        end
+    },
 
-	{
-		"nvim-telescope/telescope.nvim",
-		version = "0.1.8",  -- equivalent to tag = '0.1.8'
-		dependencies = {
-			"nvim-lua/plenary.nvim"
-		}
-	},
+    {
+        "nvim-telescope/telescope.nvim",
+        version = "0.1.8",  -- equivalent to tag = '0.1.8'
+        dependencies = {
+            "nvim-lua/plenary.nvim"
+        }
+    },
 
-	{
-		"rose-pine/neovim",
-		name = "rose-pine",  -- equivalent to 'as' in packer
-		config = function()
-			vim.cmd('colorscheme rose-pine')
-		end
-	},
+    {
+        "rose-pine/neovim",
+        name = "rose-pine",  -- equivalent to 'as' in packer
+        config = function()
+            vim.cmd('colorscheme rose-pine')
+        end
+    },
 
-	{
-		"nvim-treesitter/nvim-treesitter",
-		build = ":TSUpdate"  -- equivalent to 'run' in packer
-	},
+    {
+        "nvim-treesitter/nvim-treesitter",
+        build = ":TSUpdate"  -- equivalent to 'run' in packer
+    },
 
-	"nvim-treesitter/playground",
-	"theprimeagen/harpoon",
-	"tpope/vim-fugitive",
-	{
-		"hrsh7th/nvim-cmp",
-		-- dependencies commonly used with nvim-cmp
-		dependencies = {
-			"hrsh7th/cmp-buffer", -- buffer completions
-			"hrsh7th/cmp-path", -- path completions
-			"hrsh7th/cmp-nvim-lsp", -- LSP completions
-			"hrsh7th/cmp-cmdline", -- cmdline completions
-			"L3MON4D3/LuaSnip", -- snippet engine
-			"saadparwaiz1/cmp_luasnip", -- snippet completions
-		},
-		config = function()
-			local cmp = require("cmp")
-			cmp.setup({
-				snippet = {
-					expand = function(args)
-						require("luasnip").lsp_expand(args.body)
-					end,
-				},
-				mapping = cmp.mapping.preset.insert({
-					['<C-b>'] = cmp.mapping.scroll_docs(-4),
-					['<C-f>'] = cmp.mapping.scroll_docs(4),
-					['<C-Space>'] = cmp.mapping.complete(),
-					['<C-e>'] = cmp.mapping.abort(),
-					['<CR>'] = cmp.mapping.confirm({ select = true }),
-				}),
-				sources = cmp.config.sources({
-					{ name = 'nvim_lsp' },
-					{ name = 'luasnip' },
-					{ name = 'buffer' },
-					{ name = 'path' },
-				}),
-			})
-		end,
-	},
-	{'neovim/nvim-lspconfig'},
-	{'hrsh7th/cmp-nvim-lsp'},
-	{'hrsh7th/nvim-cmp'},
-	{
-		{
-			'williamboman/mason.nvim',
-			lazy = false,
-			opts = {},
-		},
+    "nvim-treesitter/playground",
+    "theprimeagen/harpoon",
+    "tpope/vim-fugitive",
+    {
+        "hrsh7th/nvim-cmp",
+        -- dependencies commonly used with nvim-cmp
+        dependencies = {
+            "hrsh7th/cmp-buffer", -- buffer completions
+            "hrsh7th/cmp-path", -- path completions
+            "hrsh7th/cmp-nvim-lsp", -- LSP completions
+            "hrsh7th/cmp-cmdline", -- cmdline completions
+            "L3MON4D3/LuaSnip", -- snippet engine
+            "saadparwaiz1/cmp_luasnip", -- snippet completions
+        },
+        config = function()
+            local cmp = require("cmp")
+            cmp.setup({
+                snippet = {
+                    expand = function(args)
+                        require("luasnip").lsp_expand(args.body)
+                    end,
+                },
+                mapping = cmp.mapping.preset.insert({
+                    ['<C-b>'] = cmp.mapping.scroll_docs(-4),
+                    ['<C-f>'] = cmp.mapping.scroll_docs(4),
+                    ['<C-Space>'] = cmp.mapping.complete(),
+                    ['<C-e>'] = cmp.mapping.abort(),
+                    ['<CR>'] = cmp.mapping.confirm({ select = true }),
+                }),
+                sources = cmp.config.sources({
+                    { name = 'nvim_lsp' },
+                    { name = 'luasnip' },
+                    { name = 'buffer' },
+                    { name = 'path' },
+                }),
+            })
+        end,
+    },
+    {'williamboman/mason-lspconfig.nvim'},
+    {'neovim/nvim-lspconfig'},
+    {'hrsh7th/cmp-nvim-lsp'},
+    {'hrsh7th/nvim-cmp'},
+    {
+        {
+            'williamboman/mason.nvim',
+            lazy = false,
+            opts = {},
+        },
 
-		-- Autocompletion
-		{
-			'hrsh7th/nvim-cmp',
-			event = 'InsertEnter',
-			config = function()
-				local cmp = require('cmp')
+        -- Autocompletion
+        {
+            'hrsh7th/nvim-cmp',
+            event = 'InsertEnter',
+            config = function()
+                local cmp = require('cmp')
 
-				cmp.setup({
-					sources = {
-						{name = 'nvim_lsp'},
-					},
-					mapping = cmp.mapping.preset.insert({
-						['<CR>'] = cmp.mapping.confirm({select = true}),
-						['<C-Space>'] = cmp.mapping.complete(),
-						['<C-u>'] = cmp.mapping.scroll_docs(-4),
-						['<C-d>'] = cmp.mapping.scroll_docs(4),
-						['<Tab>'] = cmp.mapping(function(fallback)
-							local col = vim.fn.col('.') - 1
+                cmp.setup({
+                    sources = {
+                        {name = 'nvim_lsp'},
+                    },
+                    preselect = 'item',
+                    completion = {
+                        completeopt = 'menu,menuone,noinsert'
+                    },
+                    mapping = cmp.mapping.preset.insert({
+                        ['<CR>'] = cmp.mapping.confirm({select = true}),
+                        ['<C-Space>'] = cmp.mapping.complete(),
+                        ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+                        ['<C-d>'] = cmp.mapping.scroll_docs(4),
+                        ['<Tab>'] = cmp.mapping(function(fallback)
+                            local col = vim.fn.col('.') - 1
 
-							if cmp.visible() then
-								cmp.select_next_item({behavior = 'select'})
-							elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
-								fallback()
-							else
-								cmp.complete()
-							end
-						end, {'i', 's'}),
-						['<S-Tab>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
-					}),
+                            if cmp.visible() then
+                                cmp.select_next_item({behavior = 'select'})
+                            elseif col == 0 or vim.fn.getline('.'):sub(col, col):match('%s') then
+                                fallback()
+                            else
+                                cmp.complete()
+                            end
+                        end, {'i', 's'}),
 
+                        -- Go to previous item
+                        ['<S-Tab>'] = cmp.mapping.select_prev_item({behavior = 'select'}),
+                    }),
+                    snippet = {
+                        expand = function(args)
+                            vim.snippet.expand(args.body)
+                        end,
+                    },
+                })
+            end
+        },
+        -- LSP
+        {
+            'neovim/nvim-lspconfig',
+            cmd = {'LspInfo', 'LspInstall', 'LspStart'},
+            event = {'BufReadPre', 'BufNewFile'},
+            dependencies = {
+                {'hrsh7th/cmp-nvim-lsp'},
+                {'williamboman/mason.nvim'},
+                {'williamboman/mason-lspconfig.nvim'},
+            },
+            init = function()
+                -- Reserve a space in the gutter
+                -- This will avoid an annoying layout shift in the screen
+                vim.opt.signcolumn = 'yes'
+            end,
+            config = function()
+                local lsp_defaults = require('lspconfig').util.default_config
 
-					snippet = {
-						expand = function(args)
-							vim.snippet.expand(args.body)
-						end,
-					},
-				})
-			end
-		},
+                -- Add cmp_nvim_lsp capabilities settings to lspconfig
+                -- This should be executed before you configure any language server
+                lsp_defaults.capabilities = vim.tbl_deep_extend(
+                'force',
+                lsp_defaults.capabilities,
+                require('cmp_nvim_lsp').default_capabilities()
+                )
 
-		-- LSP
-		{
-			'neovim/nvim-lspconfig',
-			cmd = {'LspInfo', 'LspInstall', 'LspStart'},
-			event = {'BufReadPre', 'BufNewFile'},
-			dependencies = {
-				{'hrsh7th/cmp-nvim-lsp'},
-				{'williamboman/mason.nvim'},
-				{'williamboman/mason-lspconfig.nvim'},
-			},
-			init = function()
-				-- Reserve a space in the gutter
-				-- This will avoid an annoying layout shift in the screen
-				vim.opt.signcolumn = 'yes'
-			end,
-			config = function()
-				local lsp_defaults = require('lspconfig').util.default_config
+                -- LspAttach is where you enable features that only work
+                -- if there is a language server active in the file
+                vim.api.nvim_create_autocmd('LspAttach', {
+                    desc = 'LSP actions',
+                    callback = function(event)
+                        local opts = {buffer = event.buf}
 
-				-- Add cmp_nvim_lsp capabilities settings to lspconfig
-				-- This should be executed before you configure any language server
-				lsp_defaults.capabilities = vim.tbl_deep_extend(
-				'force',
-				lsp_defaults.capabilities,
-				require('cmp_nvim_lsp').default_capabilities()
-				)
+                        vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
+                        vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
+                        vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
+                        vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
+                        vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
+                        vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
+                        vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
+                        vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
+                        vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
+                        vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
+                    end,
+                })
 
-				-- LspAttach is where you enable features that only work
-				-- if there is a language server active in the file
-				vim.api.nvim_create_autocmd('LspAttach', {
-					desc = 'LSP actions',
-					callback = function(event)
-						local opts = {buffer = event.buf}
+                require('mason-lspconfig').setup({
+                    ensure_installed = {},
+                    handlers = {
+                        -- this first function is the "default handler"
+                        -- it applies to every language server without a "custom handler"
+                        function(server_name)
+                            require('lspconfig')[server_name].setup({})
+                        end,
+                    }
+                })
+            end
+        }
+    }
+})
 
-						vim.keymap.set('n', 'K', '<cmd>lua vim.lsp.buf.hover()<cr>', opts)
-						vim.keymap.set('n', 'gd', '<cmd>lua vim.lsp.buf.definition()<cr>', opts)
-						vim.keymap.set('n', 'gD', '<cmd>lua vim.lsp.buf.declaration()<cr>', opts)
-						vim.keymap.set('n', 'gi', '<cmd>lua vim.lsp.buf.implementation()<cr>', opts)
-						vim.keymap.set('n', 'go', '<cmd>lua vim.lsp.buf.type_definition()<cr>', opts)
-						vim.keymap.set('n', 'gr', '<cmd>lua vim.lsp.buf.references()<cr>', opts)
-						vim.keymap.set('n', 'gs', '<cmd>lua vim.lsp.buf.signature_help()<cr>', opts)
-						vim.keymap.set('n', '<F2>', '<cmd>lua vim.lsp.buf.rename()<cr>', opts)
-						vim.keymap.set({'n', 'x'}, '<F3>', '<cmd>lua vim.lsp.buf.format({async = true})<cr>', opts)
-						vim.keymap.set('n', '<F4>', '<cmd>lua vim.lsp.buf.code_action()<cr>', opts)
-					end,
-				})
-
-				require('mason-lspconfig').setup({
-					ensure_installed = {},
-					handlers = {
-						-- this first function is the "default handler"
-						-- it applies to every language server without a "custom handler"
-						function(server_name)
-							require('lspconfig')[server_name].setup({})
-						end,
-					}
-				})
-			end
-		}
-	}
+vim.api.nvim_create_autocmd('TextYankPost', {
+  callback = function()
+    vim.highlight.on_yank({
+      higroup = "IncSearch",
+      timeout = 100,
+    })
+  end
 })
 
 -- Basic settings
@@ -245,7 +291,7 @@ vim.opt.smartindent = true    -- Insert indents automatically
 vim.opt.wrap = false          -- Don't wrap lines
 vim.opt.ignorecase = true     -- Ignore case in search
 vim.opt.smartcase = true      -- But don't ignore it when search string contains uppercase
-vim.opt.hlsearch = true      -- Highlight search
+vim.opt.hlsearch = false      -- Highlight search
 vim.opt.termguicolors = true  -- True color support
 vim.opt.swapfile = false
 vim.opt.incsearch = true
@@ -253,3 +299,4 @@ vim.opt.scrolloff = 8
 vim.opt.signcolumn = "yes"
 vim.opt.colorcolumn = "120"
 vim.opt.updatetime = 50
+vim.opt.cursorline = true
