27268225dcdecbc50c0f502022c6c967235ec0f9
Mukund Kidambi <mukkid@gmail.com>
2025-05-25 01:55:02 -0500

    telescope config

--- init.lua
+++ init.lua
@@ -22,6 +22,19 @@ require("lazy").setup({
         -- this is equivalent to setup({}) function
     },
     {
+        "rachartier/tiny-inline-diagnostic.nvim",
+        event = "VeryLazy", -- Or `LspAttach`
+        priority = 1000, -- needs to be loaded in first
+        config = function()
+            require('tiny-inline-diagnostic').setup(
+                {
+                    preset = "powerline"
+                }
+            )
+            vim.diagnostic.config({ virtual_text = false }) -- Only if needed in your configuration, if you already have native LSP diagnostics
+        end
+    },
+    {
         "folke/flash.nvim",
         event = "VeryLazy",
         ---@type Flash.Config
@@ -37,7 +50,7 @@ require("lazy").setup({
         -- stylua: ignore
         keys = {
             { "s", mode = { "n", "x", "o" }, function() require("flash").jump() end, desc = "Flash" },
-            { "S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
+            { "<leader>S", mode = { "n", "x", "o" }, function() require("flash").treesitter() end, desc = "Flash Treesitter" },
             { "r", mode = "o", function() require("flash").remote() end, desc = "Remote Flash" },
             { "R", mode = { "o", "x" }, function() require("flash").treesitter_search() end, desc = "Treesitter Search" },
             { "<c-s>", mode = { "c" }, function() require("flash").toggle() end, desc = "Toggle Flash Search" },
